ba154f9e0b8394df63401224c4cdafadf9dbd838
b-khadija <khadija.bayssir@gmail.com>
2023-07-28 15:20:08 +0200

    Clean clode create folder for routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/Thing');// Importation du modèle 'Thing' de Mongoose qui représente la collection dans la base de données MongoDB
 const dotEnv = require('dotenv');
 const app = express();
 dotEnv.config();
-
 const mongoUri = process.env.MONGO_URI;
+const stuffRoutes = require('./routes/stuff')
 
 // On se connecte à la base de données
 mongoose.connect(mongoUri, {
@@ -28,58 +27,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-//POST intérecepte les requetes POST
-//Enregistre un nouvel objet
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body//Recopie le contenu des objets stuff
-  });
-  thing.save().then()//Enregistre le produit dans la BDD
-    .then(() => res.status(201).json ({ message : 'Objet enregistré' }))
-    .catch(error => res.status(400).json ({ error }));
-});
-
-//Modifie un objet existant
-app.put('/api/stuff/:id', (req, res, next) => {
-  //Méthode updateOne de Mongoose pour mettre à jour un Thinf dans BDD
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message : 'Objet modifié' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-//Supprimer des données
-app.delete('/api/stuff/:id', (req, res, next) => {
-  //Méthode deleteOne() de Mongoose qui servira a supprimer 
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message : 'Objet supprimé' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-//Route GET qui permet d'aller chercher un élément avec la méthode findOne() dans notre modèle Thing avec son id 
-//On définit une route HTTP GET pour l'URL '/api/stuff/:id'. 
-//Le :id est un paramètre dynamique dans l'URL
-//Récupère un seul objet
-app.get('/api/stuff/:id', (req, res, next) => {
-  //model mongoose
-  //Recherche d'un document dans la collection 'thing'  
-  //dont l'attribut '_id' correspond à la valeur du paramètre 'id' de l'URL
-  Thing.findOne({ _id: req.params.id })
-    //Si un document est trouvé, répond avec un statut HTTP 200 (OK)
-    // et renvoie l'objet 'thing' au format JSON
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-//Récupère tous les objets
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
